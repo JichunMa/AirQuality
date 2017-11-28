9e3caa7942c5270323f4fde42e5549c30281a20b
jichunMa <jichun_ma@163.com>
2017-11-28 19:53:22 +0800

    first init

---
+++ app/src/main/java/com/example/majichun/airquality/LocationUtils.java
@@ -0,0 +1,16 @@
+package com.example.majichun.airquality;
+
+import android.content.Context;
+
+/**
+ * Created by jichunma
+ * on 2017/11/28.
+ * <p>
+ * describe:定位坐标
+ */
+
+public class LocationUtils {
+  public void init(Context context){
+
+  }
+}
