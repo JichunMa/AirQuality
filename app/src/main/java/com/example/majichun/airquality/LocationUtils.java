51135968d21a86e143154f9b5425e5bb51948a3e
jichunMa <jichun_ma@163.com>
2017-11-29 20:00:59 +0800

    import location

--- app/src/main/java/com/example/majichun/airquality/LocationUtils.java
+++ app/src/main/java/com/example/majichun/airquality/LocationUtils.java
@@ -1,6 +1,12 @@
 package com.example.majichun.airquality;
 
 import android.content.Context;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 
 /**
  * Created by jichunma
@@ -10,7 +16,46 @@ import android.content.Context;
  */
 
 public class LocationUtils {
-  public void init(Context context){
 
+  static String TAG = "LocationUtils";
+
+  public static AMapLocationClient mLocationClient = null;
+  //声明定位回调监听器
+  public static AMapLocationListener mLocationListener;
+
+  public static AMapLocationClientOption mLocationOption = null;
+
+
+
+  public static void init(Context context) {
+    mLocationListener = new AMapLocationListener() {
+      @Override
+      public void onLocationChanged(AMapLocation aMapLocation) {
+        Log.d(TAG, "onLocationChanged: " + aMapLocation.getLatitude()+", "+aMapLocation.getLongitude());
+      }
+    };
+
+  //初始化定位
+    mLocationClient = new AMapLocationClient(context.getApplicationContext());
+  //设置定位回调监听
+    mLocationClient.setLocationListener(mLocationListener);
+
+    //声明AMapLocationClientOption对象
+    //初始化AMapLocationClientOption对象
+    mLocationOption = new AMapLocationClientOption();
+    //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+    mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+    //获取一次定位结果：
+//该方法默认为false。
+    mLocationOption.setOnceLocation(true);
+    mLocationClient.setLocationOption(mLocationOption);
+
+
+  }
+
+  public static void startLocated(){
+
+    mLocationClient.startLocation();
   }
 }
--- app/src/main/java/com/example/majichun/airquality/MainActivity.java
+++ app/src/main/java/com/example/majichun/airquality/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.majichun.airquality;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    MainActivityPermissionsDispatcher.getLocationWithPermissionCheck(MainActivity.this);
   }
+
+  @NeedsPermission({Manifest.permission_group.LOCATION})
+  public void getLocation(){
+    LocationUtils.init(this);
+    LocationUtils.startLocated();
+  }
+
+  @OnPermissionDenied({Manifest.permission_group.LOCATION})
+  void showDeniedForCamera() {
+    Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show();
+    Log.d("act","Denied");
+  }
+
+  @OnNeverAskAgain({Manifest.permission_group.LOCATION})
+  void showNeverAskForCamera() {
+    Toast.makeText(this, "NeverAskFor", Toast.LENGTH_SHORT).show();
+    Log.d("act","NeverAskFor");
+  }
+
 }
